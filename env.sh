129aed62179d80c3c1b91675e203bbd5a91d3c72
champseix robin <robin.champseix@gmail.com>
2021-03-24 10:09:05 +0100

    added env var extraction automatically to create custom datasource file for grafana

---
+++ env.sh
@@ -0,0 +1,16 @@
+# InfluxDB env vars
+export INFLUXDB_USERNAME=admin
+export INFLUXDB_PASSWORD=admin
+export INFLUXDB_DATABASE=aura
+
+# Grafana env vars
+export GRAFANA_USERNAME=admin
+export GRAFANA_PASSWORD=admin
+
+# MYSQL env vars
+export MYSQL_ROOT_PASSWORD=admin
+export MYSQL_DATABASE=aura
+export MYSQL_USER=admin
+export MYSQL_PASSWORD=admin
+# For MAC OS. For others OS, you must set your private IP address.
+export MYSQL_HOST_IP=docker.for.mac.localhost
