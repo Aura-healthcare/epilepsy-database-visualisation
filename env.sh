34e9d7b6f8bf0508c1576ee3cb87cdc7418defa8
root <root@c7-2.witbe.aura>
2021-03-31 13:11:03 +0000

    updating env variables

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # InfluxDB env vars
 export INFLUXDB_USERNAME=admin
-export INFLUXDB_PASSWORD=admin
-export INFLUXDB_DATABASE=db0
+export INFLUXDB_PASSWORD=auraadmin
+export INFLUXDB_DATABASE=hackathon
 
 # Grafana env vars
 export GRAFANA_USERNAME=admin
@@ -13,10 +13,10 @@ export MYSQL_DATABASE=aura
 export MYSQL_USER=admin
 export MYSQL_PASSWORD=admin
 # For MAC OS. For others OS, you must set your private IP address.
-export MYSQL_HOST_IP=docker.for.mac.localhost
+export MYSQL_HOST_IP=$(hostname -I | cut -d ' ' -f 2)
 
 # PostgreSQL env vars
-export POSTGRES_HOST_URL=docker.for.mac.localhost
+export POSTGRES_HOST_URL=$(hostname -I | cut -d ' ' -f 3)
 export POSTGRES_DATABASE=postgres
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
